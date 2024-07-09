8926eec1784e97eaebe3515d7701d0d4aee23373
navidpl <navid_zahabi@yahoo.com>
2024-07-09 16:37:59 +0330

    session and login and htaccess

--- main.cpp
+++ main.cpp
@@ -6,133 +6,170 @@
 
 using namespace std;
 using json = nlohmann::json;
-
-void print_line(int );
+// print line of ######
+void print_line(int);
+// print main menu
 void paragraph();
-json read_from_json(string );
-int put_in_file(string,string,string,bool);
+// read json file (core.json)
+json read_from_json(string);
+// write in file
+int put_in_file(string, string, string, bool);
+//  input selection for main menu
 void mainchoose();
 int choose();
+// go back to main menu
+void back_to_main_menu();
+// print with endline before and after
+void print_common(string);
 
-
-
-
-
-vector<string> msg{"1 - DB connection", "2 - world"};
+vector<string> msg{"0 - instalation info", "1 - DB connection", "2 - create .htaccess", "3 - create session and loggedin function"};
 int choice;
-string phpStart="<?php\n";
+string phpStart = "<?php\n";
 
 int main()
 {
-    
 
-cout << endl;
+    cout << endl;
 
-print_line(2);
-paragraph();
-print_line(1);
-mainchoose();
+    print_line(2);
+    paragraph();
+    print_line(1);
+    mainchoose();
 
-
-system("mkdir core");
-system("clear");
-return 0;
-} 
-
-
-
-
-
-
-
-void mainchoose(){
-switch (choose())
-{
-case 1:
-{
-const json temp1 = read_from_json("core.json");
-cout << "DB info please:\n name of data base:";
-string dbname,username,password;
-cin >> dbname;
-cout << "\n username:";
-cin >> username;
-cout << "\n password:";
-cin >> password;
-string jeejee="$mysql_db =\""+dbname+"\"";
-jeejee += ";\n$mysql_user =\""+username+"\"";
-jeejee += ";\n$mysql_pass =\""+password+"\";\n";
-put_in_file("core/connect.inc.php",phpStart+jeejee,temp1["config"]["DB"]["connection"],0);
-//cout <<temp1["config"]["DB"]["connection"];
+    system("mkdir core");
+    system("clear");
+    return 0;
 }
+
+void mainchoose()
+{
+    switch (choose())
+    {
+    case 1:
+    {
+        const json temp1 = read_from_json("core.json");
+        cout << "DB info please:\n name of data base:";
+        string dbname, username, password;
+        cin >> dbname;
+        cout << "\n username:";
+        cin >> username;
+        cout << "\n password:";
+        cin >> password;
+        string jeejee = "$mysql_db =\"" + dbname + "\"";
+        jeejee += ";\n$mysql_user =\"" + username + "\"";
+        jeejee += ";\n$mysql_pass =\"" + password + "\";\n";
+        put_in_file("core/connect.inc.php", phpStart + jeejee, temp1["config"]["DB"]["connection"], 0);
+
+        back_to_main_menu();
+    }
     /* code */
     break;
-case 2:
-break;
-default:
-    break;
-}
-print_line(2);
-paragraph();
-print_line(1);
-mainchoose();
-}
-
-json read_from_json(string address){
-    // string line;
-  ifstream myfile(address);
-//cout <<value;
-     json config=json::parse(myfile);
-     
-    
-  
-//   if (myfile.is_open())
-//   {
-//     // while ( getline (myfile,line) )
-//     // {
-//     //   cout << line << '\n';
-//     // }
-//     myfile >> config;
-//     myfile.close();["config"]["DB"]["connection"]
-//   }
-
-
-return config;
-}
-int put_in_file(string address,string extracontent,string value,bool position){
-    if(position<1){
-value=extracontent+value;
-
-    }else{
-value=value+extracontent;
+    case 0:
+    {
+        print_line(1);
+        print_common("For DB connection write: require_once('/core/connect.inc.php'); ");
+        print_common("For Authentication write:  require_once('/core/core.inc.php'); ");
+        back_to_main_menu();
     }
-    std::ofstream outfile (address);
+    break;
+    case 2:
+    {
 
-outfile << value << std::endl;
+        const json temp1 = read_from_json("core.json");
+        put_in_file(".htaccess", "", temp1["config"]["htaccess"], 0);
 
-outfile.close();
-return 0;
+        back_to_main_menu();
+    }
+    break;
+    case 3:
+    {
+        const json temp1 = read_from_json("core.json");
+        put_in_file("core/core.inc.php", "", temp1["config"]["session"], 0);
+
+        back_to_main_menu();
+    }
+    break;
+    default:
+        break;
+    }
+    print_line(2);
+    paragraph();
+    print_line(1);
+    mainchoose();
 }
 
+json read_from_json(string address)
+{
+    // string line;
+    ifstream myfile(address);
+    // cout <<value;
+    json config = json::parse(myfile);
 
+    //   if (myfile.is_open())
+    //   {
+    //     // while ( getline (myfile,line) )
+    //     // {
+    //     //   cout << line << '\n';
+    //     // }
+    //     myfile >> config;
+    //     myfile.close();["config"]["DB"]["connection"]
+    //   }
 
-int choose(){
-    cout <<"Enter your selection :   ";
-cin >> choice;
-
-
-return choice;
+    return config;
 }
-void print_line(int times){
-for(int i=0;i<times;i++){
-printf("#############################");
-cout << endl;
+int put_in_file(string address, string extracontent, string value, bool position)
+{
+    if (position < 1)
+    {
+        value = extracontent + value;
+    }
+    else
+    {
+        value = value + extracontent;
+    }
+    std::ofstream outfile(address);
+
+    outfile << value << std::endl;
+
+    outfile.close();
+    return 0;
 }
+
+int choose()
+{
+    cout << "Enter your selection :   ";
+    cin >> choice;
+
+    return choice;
+}
+void print_line(int times)
+{
+    for (int i = 0; i < times; i++)
+    {
+        printf("#############################");
+        cout << endl;
+    }
 }
 void paragraph()
 {
-for (const string& word : msg)
+    for (const string &word : msg)
     {
-        cout <<"     "<< word << " ";
+        cout << "     " << word << " ";
         cout << endl;
     }
+}
+void back_to_main_menu()
+{
+    print_line(2);
+    cout << endl
+         << endl
+         << " For going back to main menu enter 0:";
+    string habit;
+    cin >> habit;
+}
+void print_common(string chap)
+{
+    print_line(1);
+    cout << endl
+         << "" << endl;
 }
